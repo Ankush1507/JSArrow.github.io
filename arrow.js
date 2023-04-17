274a84273f55c4b91aa88d1b70a5d36135fc409f
Ankush1507 <120745914+Ankush1507@users.noreply.github.com>
2023-04-17 20:01:38 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,17 @@
+
+// const add = (num1,num2) => (num1+num2)
+
+// const result=add(5,6);
+
+// console.log(result);
+
+
+const add= (num1,num2) => (num1+num2)
+
+
+const result = add(5,7);
+
+console.log(result);
+
+
+
